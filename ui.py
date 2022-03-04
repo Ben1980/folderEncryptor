39a525d40b2edf550025059859b4f04df27d6ee8
Benjamin Mahr <ben.mahr@gmail.com>
2022-03-04 11:27:28 +0100

    window works and is showing lable

--- ui.py
+++ ui.py
@@ -1,9 +1,17 @@
+from cProfile import label
 import wx
 
 class Encryptor(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
-        
+
+        hBox = wx.BoxSizer(wx.HORIZONTAL)
+
+        pwdLable = wx.StaticText(self, label='Generated Password')
+        hBox.Add(pwdLable, 0, wx.ALL, 5)
+        self.SetSizer(hBox)
+        hBox.Fit(parent)
+        self.Layout()
 
 class MainWindow(wx.Frame):
     def __init__(self):
