61b3d65c0c2a5ab87c8a21156594b63a9de7f4f7
Benjamin Mahr <ben.mahr@gmail.com>
2022-03-24 13:59:05 +0100

    progress bar added

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import os
 import sys
 import random
 import string
+import wx
 
 def generatePassword():
     length = 45                      
@@ -29,20 +30,38 @@ def encryptFolder(full_path):
     zip_name = full_path.replace(root_path + '\\', '')
     zip_name = root_path + '\\' + zip_name + '.zip'
 
+    nb_files = 0;
+    for root, folders, files in os.walk(full_path):
+        nb_files += len(files)
+
     try:
+        dialog = wx.ProgressDialog("File encryption in progress...", "Time remaining", nb_files, style=wx.PD_CAN_ABORT | wx.PD_ELAPSED_TIME | wx.PD_REMAINING_TIME)
+        keepGoing = True
+
         with pyzipper.AESZipFile(zip_name, 'w', compression=pyzipper.ZIP_LZMA) as zf:
             zf.setpassword(password)
             zf.setencryption(pyzipper.WZ_AES, nbits=256)
+            count = 0
             for root, folders, files in contents:
                 for folder_name in folders:
                     absolute_path = os.path.join(root, folder_name)
                     relative_path = absolute_path.replace(root_path + '\\', '')
                     zf.write(absolute_path, relative_path)
+                    if not keepGoing:
+                        break
                 for file_name in files:
                     absolute_path = os.path.join(root, file_name)
                     relative_path = absolute_path.replace(root_path + '\\','')
                     zf.write(absolute_path, relative_path)
+                    count = count + 1
+                    keepGoing  = dialog.Update(count)
+                    if not keepGoing:
+                        break
 
+                if not keepGoing:
+                        break
+
+        dialog.Destroy()
     except IOError as message:
         print (message)
         sys.exit(1)
